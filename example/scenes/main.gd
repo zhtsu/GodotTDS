83cdfcc748ec775ca2d5ba691181384a90898077
zhtsu <zhtsu@qq.com>
2024-08-05 03:12:32 +0800

    Anti addiction is complete

--- example/scenes/main.gd
+++ example/scenes/main.gd
@@ -2,13 +2,13 @@ extends CanvasLayer
 
 
 func _ready() -> void:
-	GodotTDS.on_login_success.connect(_on_login_success)
+	GodotTDS.on_anti_addiction_return.connect(_on_test_return)
 
 
 func _on_button_button_down() -> void:
-	GodotTDS.login()
+	GodotTDS.anti_addiction()
 	
 	
-func _on_login_success(user_profile : Dictionary):
-	$Label.text = user_profile["nickName"]
+func _on_test_return(code : int, msg : String):
+	$Label.text = msg
 	
